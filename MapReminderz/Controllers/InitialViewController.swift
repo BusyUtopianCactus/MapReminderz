b1674a05018655967f267c77109452873dc0f80f
Daniel Basman <dbasman2@gmail.com>
2018-07-16 16:27:06 -0700

    Using reminderz instead of reminder

---
+++ MapReminderz/Controllers/InitialViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MapReminderz
+//
+//  Created by Ilana Basman on 7/16/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class InitialViewController : UIViewController {
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+}
+
+extension InitialViewController : CLLocationManagerDelegate {
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            
+            print("location:: \(location)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error)")
+    }
+}
