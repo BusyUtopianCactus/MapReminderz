4c99f42327a4c1928e453ff354d2a4033cd64cdd
Alex <alexander.p.niehaus@gmail.com>
2018-07-17 14:29:18 -0700

    General ChecklistTabViewController

---
+++ MapReminderz/Controllers/CheckListTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CheckListTableViewController.swift
+//  MapReminderz
+//
+//  Created by Alexander Niehaus on 7/17/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckListTabViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var reminderLabel: UILabel!
+    
+    @IBOutlet weak var oneTextField: UITextField!
+    @IBOutlet weak var twoTextField: UITextField!
+    @IBOutlet weak var threeTextField: UITextField!
+    @IBOutlet weak var fourTextField: UITextField!
+    @IBOutlet weak var fiveTextField: UITextField!
+    @IBOutlet weak var sixTextField: UITextField! 
+    
+}
+
